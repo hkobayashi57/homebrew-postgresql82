80a349fa4412d71ce14c5a037cdd650674706fd9
小林　弘和 <kobayashihirokazu@kobayashikouwanoMacBook-Air.local>
2017-10-27 17:37:08 +0900

    sha1 -> sha256

---
+++ install_pg-8.2.sh
@@ -0,0 +1,4 @@
+brew install https://gist.github.com/raw/4355097/postgresql82.rb --no-python --no-perl --without-ossp-uuid
+
+# to execute this gist, just run the line bellow in terminal
+# \curl -L https://gist.github.com/raw/4355097/install_pg-8.2.sh | sh
